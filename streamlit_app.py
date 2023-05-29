0af6b3a0cd94894f7462c637eb2ce1d022889a95
yuxingnan <monkey_yxn@yahoo.cn>
2023-05-29 20:11:55 +0800

    Update streamlit_app.py
    added header 
    added other texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & BlueBerry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
