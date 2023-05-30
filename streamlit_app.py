c358227d14a47970133486654d3f7c14c55385dd
yuxingnan <monkey_yxn@yahoo.cn>
2023-05-30 22:24:43 +0800

    Update streamlit_app.py
    udpated dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free Range Egg')
 
 streamlit.header('🍌 Build Your Own Fruit Smoothie ')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
