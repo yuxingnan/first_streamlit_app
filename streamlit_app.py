a1d05930af148473017b9c0fcbe0a2eec29fb65d
yuxingnan <monkey_yxn@yahoo.cn>
2023-06-04 00:40:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,7 @@ def get_fruit_load_list():
        my_cur_execute("select * from furit_load_list") 
        return my_cur.fetchall()
 
+if streamlit.button('Get Fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
